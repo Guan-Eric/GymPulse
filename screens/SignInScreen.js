5243925e618a6dbfaeb4811524242ee8bdc036c7
Guan-Eric <erictheguan@gmail.com>
2023-12-22 18:03:27 -0500

    Create new screens

---
+++ screens/SignInScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignInScreen(props) {
+  return <div></div>;
+}
+
+export default SignInScreen;
---
+++ screens/SignupScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SignupScreen(props) {
+  return <div></div>;
+}
+
+export default SignupScreen;
