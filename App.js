8f79156d91dfb1775b679d8b92885a4182af7eed
Guan-Eric <60152251+Guan-Eric@users.noreply.github.com>
2023-12-22 11:24:29 -0500

    Add logo.png

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './screens/WelcomeScreen';
 
 export default function App() {
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Image, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 function WelcomeScreen(props) {
     return (
-        <ImageBackground>
+        <View style={styles.container}>
+            <LinearGradient
+            colors={['#2a2a2a', '#111111' ]}
             style={styles.background}
-            source={require("../assets/welcome.png")}
-        </ImageBackground>
+            start={{ x: 1, y: 0 }}>
+                <SafeAreaView style={styles.container}>
+                    <Image style={{
+                            resizeMode: 'contain',
+                            height: 100,
+                            width: 100,
+                        }}
+                        source={require('../assets/logo.png')}/>
+                    <Text>FitAI</Text>
+                    <Text>Sign in</Text>    
+                </SafeAreaView>
+            </LinearGradient>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    background: {
+    container: {
         flex: 1,
-
-    }
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      background: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        height: '100%'
+      }
 })
 
 export default WelcomeScreen;
