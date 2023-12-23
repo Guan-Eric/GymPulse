a4542ca3da04484305f71291f5445e840d976185
Guan-Eric <60152251+Guan-Eric@users.noreply.github.com>
2023-12-23 01:12:59 -0500

    Update welcome page

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,42 +1,68 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, View, Image, Text } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  Button,
+  Alert,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 function WelcomeScreen(props) {
-    return (
-        <View style={styles.container}>
-            <LinearGradient
-            colors={['#2a2a2a', '#111111' ]}
-            style={styles.background}
-            start={{ x: 1, y: 0 }}>
-                <SafeAreaView style={styles.container}>
-                    <Image style={{
-                            resizeMode: 'contain',
-                            height: 100,
-                            width: 100,
-                        }}
-                        source={require('../assets/logo.png')}/>
-                    <Text>FitAI</Text>
-                    <Text>Sign in</Text>    
-                </SafeAreaView>
-            </LinearGradient>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        colors={["#2a2a2a", "#111111", "#000000"]}
+        style={styles.background}
+        start={{ x: 1, y: 0 }}
+      >
+        <SafeAreaView style={styles.container}>
+          <Image
+            style={{
+              resizeMode: "contain",
+              height: 100,
+              width: 100,
+            }}
+            source={require("../assets/logo.png")}
+          />
+          <Text style={styles.logoText}>FitAI</Text>
+          <Text style={styles.baseText}>Your Personal Trainer</Text>
+          <Button title="Sign In" onPress={() => Alert.alert("Sign In")} />
+          <Button title="Sign Up" onPress={() => Alert.alert("Sign Up")} />
+        </SafeAreaView>
+      </LinearGradient>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      background: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        height: '100%'
-      }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: "100%",
+  },
+  baseText: {
+    fontFamily: "Gotham",
+    color: "white",
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  logoText: {
+    color: "#3490de",
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+});
 
 export default WelcomeScreen;
