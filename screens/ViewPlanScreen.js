19453cb0a3f94c5d3650c0fff981aa588da3dac4
Guan-Eric <erictheguan@gmail.com>
2024-01-21 00:09:22 -0500

    Add delete days

--- screens/ViewPlanScreen.js
+++ screens/ViewPlanScreen.js
@@ -11,14 +11,23 @@ import { darkMode } from "../styles/darkMode";
 import { lightMode } from "../styles/lightMode";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FIRESTORE_DB } from "../firebaseConfig";
-import { updateDoc, getDoc, doc, collection, addDoc } from "firebase/firestore";
+import {
+  updateDoc,
+  getDoc,
+  doc,
+  collection,
+  addDoc,
+  getDocs,
+  deleteDoc,
+} from "firebase/firestore";
 
 function ViewPlanScreen({ route }) {
   Appearance.getColorScheme() == "light"
     ? (styles = lightMode)
     : (styles = darkMode);
-  const [name, setName] = useState();
-  const [plan, setPlan] = useState();
+  const [name, setName] = useState("");
+  const [plan, setPlan] = useState({});
+  const [days, setDays] = useState([]);
   const [exercises, setExercises] = useState();
   useEffect(() => {
     const fetchPlanFromFirestore = async () => {
@@ -28,6 +37,12 @@ function ViewPlanScreen({ route }) {
         );
         const planData = planDoc.data();
         setPlan(planData);
+
+        const daysCollection = collection(planDoc.ref, "Days");
+        const daysSnapshot = await getDocs(daysCollection);
+        const daysData = daysSnapshot.docs.map((doc) => doc.data());
+        setDays(daysData);
+
         console.log("plan");
       } catch (error) {
         console.error("Error fetching plan data:", error);
@@ -48,8 +63,24 @@ function ViewPlanScreen({ route }) {
       name: "New Day",
       planId: route.params.item.id,
     });
-    const dayDoc = doc(FIRESTORE_DB, `Days/${daysDocRef.id}`);
+    const dayDoc = doc(daysCollection, daysDocRef.id);
     await updateDoc(dayDoc, { id: daysDocRef.id });
+    const daysSnapshot = await getDocs(daysCollection);
+    const daysData = daysSnapshot.docs.map((doc) => doc.data());
+    setDays(daysData);
+  };
+
+  const handleDeleteDay = async (dayId) => {
+    try {
+      const dayDocRef = doc(
+        FIRESTORE_DB,
+        `Plans/${route.params.item.id}/Days/${dayId}`
+      );
+      await deleteDoc(dayDocRef);
+      setDays((prevDays) => prevDays.filter((day) => day.id !== dayId));
+    } catch (error) {
+      console.error("Error deleting day:", error);
+    }
   };
   return (
     <View style={styles.container}>
@@ -62,7 +93,12 @@ function ViewPlanScreen({ route }) {
           value={name}
         />
         <ScrollView>
-          {}
+          {days.map((day) => (
+            <View key={day.id} style={styles.dayContainer}>
+              <Text style={styles.titleText}>{day.name}</Text>
+              <Button title="Delete" onPress={() => handleDeleteDay(day.id)} />
+            </View>
+          ))}
           <Button title="Add Day" onPress={handleAddDay} />
         </ScrollView>
       </SafeAreaView>
