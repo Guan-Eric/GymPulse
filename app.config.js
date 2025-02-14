bc23249c5e9e384201eeea7bb8cd74b2b4b43264
guan-eric <erictheguan@gmail.com>
2025-02-14 10:30:41 -0500

    Update Icon

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ export default {
   expo: {
     name: "Gym Pulse",
     slug: "gym-pulse",
-    version: "1.3.8",
+    version: "1.3.10",
     scheme: "your-app-scheme",
     orientation: "portrait",
     icon: "./assets/icon.png",
